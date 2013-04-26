3cb0fced4440ef9d035725c10624d018e05df206
hauss <philhaussler@gmail.com>
2013-04-26 12:47:59 -0500

    Add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	@pins = @user.pins
+  	@pins = @user.pins.order("created_at desc").page(params[:page]).per_page(20)
   end
 end
